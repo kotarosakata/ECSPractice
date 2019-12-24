6c5f5f82dfd9106db7570adf7b61f0744261c06d
kotarosakata <kotarosakata2002@gmail.com>
2019-12-24 17:47:24 +0900

    make commit

---
+++ Assets/FleetMove.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+public class FleetMove : ComponentSystem
+{
+    private float speed = 100f;
+    // Start is called before the first frame update
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        this.query = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[] { ComponentType.ReadWrite<Translation>() },
+        });
+    }
+
+    protected override void OnUpdate()
+    {
+        var time = Time.realtimeSinceStartup;
+        Entities.ForEach((ref Translation translation) => { translation.Value.z += speed*Time.deltaTime; });
+    }
+
+    EntityQuery query;
+}
---
+++ Assets/SimpleECSTest.cs
@@ -0,0 +1,72 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public class SimpleECSTest : MonoBehaviour
+{
+    [SerializeField] private Mesh _mesh;
+    [SerializeField] private Material _material;
+    void Start()
+    {
+        CreateCube();
+    }
+
+    void CreateCube()
+    {
+        var manager = World.Active.EntityManager;
+        // Entity が持つ Components を設計
+        var archetype = manager.CreateArchetype(
+            ComponentType.ReadWrite<LocalToWorld>(), 
+            ComponentType.ReadWrite<Translation>(),
+            ComponentType.ReadOnly<RenderMesh>());
+        // 上記の Components を持つ Entity を作成
+        var entity = manager.CreateEntity(archetype);
+        // Entity の Component の値をセット（位置）
+     //   manager.SetComponentData(entity, new Translation() { Value = new float3(0, 1, 0) });
+
+        // キューブオブジェクトの作成
+
+        
+        // Entity の Component の値をセット（描画メッシュ）
+       
+
+       
+       
+        const int SIDE = 10;
+        const int xSIDE = 100;
+        using (NativeArray<Entity> entities = new NativeArray<Entity>(SIDE * xSIDE*SIDE, Allocator.Temp, NativeArrayOptions.UninitializedMemory))
+        {
+            // Prefab Entity をベースに 10000 個の Entity を作成
+            manager.Instantiate(entity, entities);
+            // 平面に敷き詰めるように Translation を初期化
+            for (int x = 0; x < xSIDE; x++)
+            {
+                for (int y = 0; y < SIDE; y++)
+                {
+                    for (int z = 0; z < SIDE; z++)
+                    {
+                        int index = x + y * xSIDE + z * SIDE * xSIDE;
+                        manager.SetComponentData(entities[index], new Translation
+                        {
+                            Value = new float3(x*100, y*60, z*200)
+                        });
+                        manager.SetSharedComponentData(entities[index], new RenderMesh()
+                        {
+                            mesh =_mesh,
+                            material =_material,
+                            subMesh = 0,
+                            castShadows = UnityEngine.Rendering.ShadowCastingMode.Off,
+                            receiveShadows = false
+                        });
+                    }
+
+                }
+            }
+        }
+        
+        // キューブオブジェクトの削除
+    }
+}
